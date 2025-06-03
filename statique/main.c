8cf20785594456d2d2a9bb9bc71ce352fbbd6a12
dureldjiokou <jouanndureldjiokou@icloud.com>
2025-06-03 11:03:38 +0000

    exo: statique

--- statique/main.c
+++ statique/main.c
@@ -1,13 +1,13 @@
-   #include <stdio.h>
-   int sum (int num) {
-       /**
-       * find sum to n numbers
-       */
-   }
+#include <stdio.h>
 
-   int main() {
-       printf("%d ",sum(55));
-       printf("%d ",sum(45));
-       printf("%d ",sum(50));
-       return 0;
-   }
+int sum(int num) {
+    return (num * (num + 1)) / 2;
+}
+
+int main() {
+    printf("%d ", sum(55));  // Affiche 1540
+    printf("%d ", sum(45));  // Affiche 1035
+    printf("%d ", sum(50));  // Affiche 1275
+    return 0;
+}
+
