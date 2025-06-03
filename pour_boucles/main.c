7977bc8676989a9222c412408d7795d3750d703a
dureldjiokou <jouanndureldjiokou@icloud.com>
2025-06-03 10:36:39 +0000

    exo: boucle For

--- pour_boucles/main.c
+++ pour_boucles/main.c
@@ -5,7 +5,13 @@ int main() {
   int factorial = 1;
   int i;
 
-  /* calculate the factorial using a for loop  here*/
+  // Calculer 10! = 1 × 2 × 3 × ... × 10
+  for (i = 0; i < 10; i++) {
+    factorial *= array[i];  // équivaut à factorial = factorial * array[i];
+  }
 
   printf("10! is %d.\n", factorial);
+
+  return 0;
 }
+
