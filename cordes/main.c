9fdb957cdeb392817b7118dc9ff04d4081a2cc1e
dureldjiokou <jouanndureldjiokou@icloud.com>
2025-06-03 10:25:15 +0000

    exo: cordes

--- cordes/main.c
+++ cordes/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-  /* define first_name */
-  /* define last_name */
+  char first_name[] = "John";
+  char last_name[] = "Boe";
   char name[100];
 
-  last_name[0] = 'B';
+  // Construire "John Boe"
   sprintf(name, "%s %s", first_name, last_name);
+
   if (strncmp(name, "John Boe", 100) == 0) {
       printf("Done!\n");
   }
+
+  // Vider et concaténer partiellement
   name[0]='\0';
-  strncat(name,first_name,4);
-  strncat(name,last_name,20);
-  printf("%s\n",name);
+  strncat(name, first_name, 4);  // "John"
+  strncat(name, last_name, 20);  // "Boe" → résultat: "JohnBoe"
+  printf("%s\n", name);
+
   return 0;
 }
+
